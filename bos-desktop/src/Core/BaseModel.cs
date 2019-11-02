33118deb4ca757759c797e533c0fc9245d9b8bd4
lahiruramesh <lahiru@insureme.lk>
2019-11-02 11:36:48 +0530

    some modification

--- bos-desktop/src/Core/BaseModel.cs
+++ bos-desktop/src/Core/BaseModel.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using bos_desktop.src.Modules.Database;
+using MySql.Data.MySqlClient;
 
 namespace bos_desktop.src.Core
 {
 	class BaseModel
 	{
+		public MySqlConnection dbConnection;
+		public MySqlCommand command;
 
+		public BaseModel()
+		{
+			this.dbConnection = Connection.GetInstance().GetConnection();
+			this.dbConnection.Open();
+			this.command = this.dbConnection.CreateCommand();
+		}
+
+		public Int64 Insert(String table, Array data)
+		{
+
+			return 0;
+		}
+
+
+		public bool Update(String table, Array where, Array data)
+		{
+			return false;	
+		}
+
+
+		public bool Remove(String table, Int64 id, Array where) 
+		{
+			return false;
+		}
+
+		public String[] SelectItem(String table, Array where)
+		{
+			return new String[0];
+		}
+
+		public void selectMultiple(String table, Array where)
+		{ 
+			
+		}
 	}
 }
---
+++ bos-desktop/src/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace bos_desktop.src.Models
+{
+	struct User
+	{
+		public string Database { get; set; }
+		public string Table { get; set; }
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Status { get; set; }
+
+		static User() { }
+	}
+}
